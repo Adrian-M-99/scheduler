69e5908f8365a621c0dfa58d6b97e197111137d0
Adrian <myself>
2021-11-04 19:38:44 +0200

    initial commit - test

---
+++ src/main/java/com/example/scheduler21/Scheduler21Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduler21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scheduler21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Scheduler21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/scheduler21/Scheduler21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduler21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scheduler21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
